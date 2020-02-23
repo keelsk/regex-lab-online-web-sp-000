923628524a0f79c2f3a973a47e13c7058ac2916d
Kirsten Keels <keelsk@gmail.com>
2020-02-23 00:19:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,22 @@
 def starts_with_a_vowel?(word)
-
+  if((word.match(/\b[aeiou]/))||(word.match(/\b[AEIOU]/)))
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\bun\w+ing/)
 
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/\A[A-Z][[:punct]]$/)
 end
 
 def valid_phone_number?(phone)
