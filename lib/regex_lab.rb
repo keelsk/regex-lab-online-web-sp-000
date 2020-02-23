80af39f200830651f2ea1563fefa3b4b258ec1f1
Kirsten Keels <keelsk@gmail.com>
2020-02-23 01:15:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,5 +24,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if(text.match(/[(]\d{3}[)]\d{7}/) || text.match(/[(]\d{3}[)]\d{7}/))
+  if(text.match(/[(]\d{3}[)]\d{7}/) || text.match(/[(]\d{3}[)]\d{3}-\d{4}/) || )
 end
